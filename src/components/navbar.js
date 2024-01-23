07ac228d2678303195c37c5944341bb5a8c8a9a7
Brandon Bozeman <128614590+YutskiLGC@users.noreply.github.com>
2024-01-23 17:59:46 -0500

    addressed navbar issues around names stacking over one another. Routes are ready to be made

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,23 +2,24 @@ import "./Navbar.css"
 export function Navbar() {
     return <nav className = "nav">
     <a href= "/" className= "site-title">Group Portfolio Project!</a>
-    <ul>
+    
     <li>
         <a href="/Colin">Colin</a>
-    </li><li>
+    </li>
+    <li>
         <a href="/Brandon">Brandon</a>
-        </li>
-        <li>
+    </li>
+    <li>
         <a href="/Douglas">Douglas </a>
-        </li>
-        <li>
+    </li>
+    <li>
         <a href="/Rob">Rob </a>
-        </li>
-        <li>
+    </li>
+    <li>
         <a href="/Sam">Sam </a>
-        </li>
+    </li>
 
-    </ul>
+    
 
     </nav>
 }
